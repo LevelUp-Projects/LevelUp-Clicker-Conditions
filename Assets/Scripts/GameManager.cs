34ae6a26bb33496943e5ad6b363513fb7cd6be99
Martin Piliar <edino525@gmail.com>
2021-01-28 14:50:20 +0100

    Added solution

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,19 +8,30 @@ public class GameManager : MonoBehaviour
 {
     [Header("Player data")]
     //Player data (total coins, per click, per second)
+    public double playerCoins;
+    public float coinsPerClick;
+    public float coinsPerSecond;
 
     [Header("Prices")]
     //Total coin prices
+    public float coinsPerClickPrice;
+    public float coinsPerSecondPrice;
 
     [Header("Price multipliers")]
     //Price multipliers (e.g. x1.2 etc..)
+    public float multiplierCoinsPerClickPrice;
+    public float multiplierCoinsPerSecondPrice;
 
     [Header("Coin Multiplier values")]
     //Total multiplier values
+    public float multiplierCoinsPerClick;
+    public float multiplierCoinsPerSecond;
 
     [Header("Base coin multiplier values")]
     //Not needed
     // Here we can set the multiplier of the multiplier
+    public float baseMultiplierCoinsPerClick;
+    public float baseMultiplierCoinsPerSecond;
 
     [Header("Text objects")]
     //UI stuff
@@ -35,6 +46,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Timer")]
     //Total timer and the click interval
+    private float timer = 0;
+    private float timerInterval = 1;
 
     [Header("Colors")]
     //Colors to show depending on current coin state
@@ -49,17 +62,38 @@ public class GameManager : MonoBehaviour
         //Timer
         //Add coins on timer
         //Add coins on click
+        timer += Time.deltaTime;
+        if(timer >= timerInterval)
+        {
+            timer = 0;
+            playerCoins += coinsPerSecond;
+            UpdateUI();
+        }
+        if (Input.GetMouseButtonDown(0) && !CanClick())
+        {
+            playerCoins += coinsPerClick;
+            UpdateUI();
+        }
     }
 
     private void UpdateUI()
     {
         //Set UI texts according to the data
+        playerCoinsText.text = "MY COINS: " + playerCoins.ToString("0.#");
+        coinsPerClickText.text = "Coins per click: " + coinsPerClick.ToString("0.#");
+        coinsPerSecondText.text = "Coins per second: " + coinsPerSecond.ToString("0.#");
+        pricePerClickText.text = coinsPerClickPrice.ToString("0.#") + " Coins";
+        pricePerSecondText.text = coinsPerSecondPrice.ToString("0.#") + " Coins";
+        multiplierCoinsPerClickText.text = "+" + multiplierCoinsPerClick.ToString("0.#");
+        multiplierCoinsPerSecondText.text = "+" + multiplierCoinsPerSecond.ToString("0.#");
         //Call the color change
         SetButtonTextColors();
     }
     private void SetButtonTextColors()
     {
         //Set the coin price color according to the difference between player's coins and the price
+        pricePerClickText.color = playerCoins >= coinsPerClickPrice ? greenColor : redColor;
+        pricePerSecondText.color = playerCoins >= coinsPerSecondPrice ? greenColor : redColor;
     }
     public void UpgradeCoinsPerClick()
     {
@@ -68,6 +102,18 @@ public class GameManager : MonoBehaviour
         //Subtract coins from the player according to the price
         //Upgrade price
         //Upgrade UI
+        if(playerCoins >= coinsPerClickPrice)
+        {
+            coinsPerClick *= multiplierCoinsPerClick;
+            playerCoins -= coinsPerClickPrice;
+            coinsPerClickPrice *= multiplierCoinsPerClickPrice;
+            multiplierCoinsPerClick *= baseMultiplierCoinsPerClick;
+            UpdateUI();
+        }
+        else
+        {
+            print("Not enough coins");
+        }
     }
     public void UpgradeCoinsPerSecond()
     {
@@ -76,6 +122,18 @@ public class GameManager : MonoBehaviour
         //Subtract coins from the player according to the price
         //Upgrade price
         //Upgrade UI
+        if (playerCoins >= coinsPerSecondPrice)
+        {
+            coinsPerSecond *= multiplierCoinsPerSecond;
+            playerCoins -= coinsPerSecondPrice;
+            coinsPerSecondPrice *= multiplierCoinsPerSecondPrice;
+            multiplierCoinsPerSecond *= baseMultiplierCoinsPerSecond;
+            UpdateUI();
+        }
+        else
+        {
+            print("Not enough coins");
+        }
     }
     private bool CanClick()
     {
